0851fccb591e22ee3fb1974f69189eda186442e0
otto <ottofy@zohomail.com>
2023-04-12 04:24:16 -0600

    Create Scroll top button

--- src/index.js
+++ src/index.js
@@ -4,17 +4,30 @@ import "./styles/main.css";
 import "./styles/footer.css";
 import copyToClipboard from "./utilities/copyToClipboard";
 
-
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-
 const header = $(".header");
 const lines = $(".lines");
+const scrollTop = $(".scroll-top");
 // reverb thing fl studio
 const handleCopyEmail = () => copyToClipboard('ottofy@zohomail.com');
 const handleCopyPhone = () => copyToClipboard('9709882548');
+
 const handlescroll = () => {
-  window.pageYOffset > 0 ? header.classList.add("header-filter") : header.classList.remove("header-filter");
+  if (window.pageYOffset > 0) {
+    header.classList.add("header-filter");
+    scrollTop.classList.remove("hide-scroll-top");
+  } else {
+    header.classList.remove("header-filter");
+    scrollTop.classList.add("hide-scroll-top");
+  }
+};
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 };
 
 const animateLines = () => {
@@ -35,11 +48,12 @@ const animateLines = () => {
       }
     }
 
-    lines.style.opacity = `.${opac}`
-  }, 50)
-}
+    lines.style.opacity = `.${opac}`;
+  }, 50);
+};
 
 window.addEventListener("scroll", handlescroll);
 $(".copy-email").onclick = handleCopyEmail;
 $(".copy-phone").onclick = handleCopyPhone;
+scrollTop.onclick = scrollToTop;
 animateLines();
