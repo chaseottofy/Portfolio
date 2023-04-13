198ed077ad0ca0cc662b30c8cfb33df200b3a914
otto <ottofy@zohomail.com>
2023-04-13 04:52:06 -0600

    Create Toast and Copy to Clipboard features

--- src/index.js
+++ src/index.js
@@ -8,28 +8,26 @@ import "./styles/footer.css";
 import "./styles/aside/scrolltop.css";
 import "./styles/aside/themeMenu.css";
 import "./styles/aside/contactMenu.css";
+import "./styles/aside/toast.css";
 import "./styles/gradientElements/headerGradient.css";
 import "./styles/gradientElements/dividerGradient.css";
 
 import delegateTheme, { initTheme } from "./utilities/handleTheme";
 import handleScroll from "./utilities/handleScroll";
-import copyToClipboard from "./utilities/copyToClipboard";
+import initCopy from "./utilities/copyToClipboard";
 import setMenuLinks from "./utilities/handleMenuLinks";
+
+
 const $ = document.querySelector.bind(document);
 
 window.addEventListener("load", initTheme, { once: true });
 window.addEventListener("scroll", handleScroll);
+
 $(".theme-switch").addEventListener("click", delegateTheme);
-
-// $(".copy-email").addEventListener("click", () => {
-//   copyToClipboard('ottofy@zohomail.com');
-// });
-
-// $(".copy-phone").addEventListener("click", () => {
-//   copyToClipboard('9709882548');
-// });
 
 $(".scroll-top").addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+initCopy();
 setMenuLinks();
--- src/utilities/copyToClipboard.js
+++ src/utilities/copyToClipboard.js
@@ -1,11 +1,83 @@
-/**
- * 
- * @param {string} text hard-coded text to copy to clipboard (phone #/email/etc.)
- * @returns 
- */
+const $ = document.querySelector.bind(document);
+const toastWrapper = $(".toast-wrapper");
+
+let toastIndex = 0;
+const handleToasts = () => {
+  console.log(toastIndex)
+  if (toastWrapper.children.length >= 4) {
+    toastWrapper.removeChild(toastWrapper.children[toastWrapper.children.length - 1]);
+  }
+
+  for (let i = 0; i < toastWrapper.children.length; i++) {
+    const toast = toastWrapper.children[i];
+    const idx = +toast.getAttribute("toast-idx");
+    if (idx === 4) {
+      toast.classList.add("hide-toast");
+    }
+
+    toast.setAttribute("class", `toast toast--${i + 1}`);
+    toast.style.zIndex = 9000 - i;
+    toast.style.bottom = `${i + 2}rem`;
+  }
+};
+
+const createToast = (text) => {
+  if (toastIndex <= 3) {toastIndex++;}
+
+  let width = 0;
+  const wrapper = document.createElement("aside");
+  wrapper.classList.add("toast");
+  wrapper.style.zIndex = 9000;
+  wrapper.style.bottom = '2rem';
+  wrapper.setAttribute("toast-idx", toastIndex);
+
+  const progressbar = document.createElement("span");
+  progressbar.classList.add("toast-progress");
+  progressbar.style.width = `${width}%`;
+
+  const progresstrack = document.createElement("span");
+  progresstrack.classList.add("toast-progress--length");
+
+  const message = document.createElement("span");
+  message.classList.add("toast-message");
+  message.textContent = `copied – ${text}`;
+
+  wrapper.append(progressbar, progresstrack, message);
+  toastWrapper.prepend(wrapper);
+  handleToasts();
+
+  setInterval(() => {
+    width++;
+    progressbar.style.width = `${width}%`;
+    if (width === 100) {
+      wrapper.remove();
+      if (toastIndex > 0) {toastIndex--;}
+      clearInterval();
+    }
+  }, 10);
+};
+
 const copyToClipboard = (text) => {
   if (!navigator.clipboard || !text) return;
   return navigator.clipboard.writeText(text);
 };
 
-export default copyToClipboard;
+const handleCopyEmail = () => {
+  const message = "ottofy@zohomail.com";
+  copyToClipboard(message);
+  createToast(message);
+  return;
+}
+
+// const handleCopyPhone = () => {
+//   const message = "9709882548";
+//   copyToClipboard(message);
+//   createToast(message);
+//   return;
+// }
+const initCopy = () => {
+  $(".copy-email").addEventListener("click", handleCopyEmail);
+  // $(".copy-phone").addEventListener("click", handleCopyPhone);
+}
+
+export default initCopy;
