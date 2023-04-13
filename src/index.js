4ee40ba85e959bd9724f5e139a25772bee831e17
otto <ottofy@zohomail.com>
2023-04-13 03:50:36 -0600

    Create Theme Modal

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import "./styles/header.css";
 import "./styles/main.css";
 import "./styles/footer.css";
 import "./styles/aside/scrolltop.css";
+import "./styles/aside/themeMenu.css";
+import "./styles/aside/contactMenu.css";
 import "./styles/gradientElements/headerGradient.css";
 import "./styles/gradientElements/dividerGradient.css";
 
@@ -18,12 +20,15 @@ const $ = document.querySelector.bind(document);
 window.addEventListener("load", initTheme, { once: true });
 window.addEventListener("scroll", handleScroll);
 $(".theme-switch").addEventListener("click", delegateTheme);
-$(".copy-email").addEventListener("click", () => {
-  copyToClipboard('ottofy@zohomail.com');
-});
-$(".copy-phone").addEventListener("click", () => {
-  copyToClipboard('9709882548');
-});
+
+// $(".copy-email").addEventListener("click", () => {
+//   copyToClipboard('ottofy@zohomail.com');
+// });
+
+// $(".copy-phone").addEventListener("click", () => {
+//   copyToClipboard('9709882548');
+// });
+
 $(".scroll-top").addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
--- src/utilities/copyToClipboard.js
+++ src/utilities/copyToClipboard.js
@@ -7,4 +7,5 @@ const copyToClipboard = (text) => {
   if (!navigator.clipboard || !text) return;
   return navigator.clipboard.writeText(text);
 };
+
 export default copyToClipboard;
