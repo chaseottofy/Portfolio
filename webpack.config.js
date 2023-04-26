a294601bc1cbdbb240e32a22426f5d0144983db5
biodiscus <biodiscus@gmail.com>
2023-04-26 19:49:33 +0200

    Optimize webpack config for html-bundler-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackInjectPreload = require('@principalstudio/html-webpack-inject-preload');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
@@ -9,12 +7,18 @@ const svgToMiniDataURI = require("mini-svg-data-uri");
 
 module.exports = (env) => {
   return {
-    entry: './src/index.js',
     mode: 'development',
     devtool: 'source-map',
     devServer: {
       static: {
         directory: path.join(__dirname, 'dist'),
+      },
+      // enable live reload
+      watchFiles: {
+        paths: ['src/**/*.*'],
+        options: {
+          usePolling: true,
+        },
       },
       port: 3000,
       open: true,
@@ -24,29 +28,25 @@ module.exports = (env) => {
     module: {
       rules: [
         {
-          test: /\.html$/i,
-          loader: "html-loader",
-        },
-        {
           test: /\.js$/,
           enforce: "pre",
           exclude: /node_modules/,
           use: ["source-map-loader"],
         },
         {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
+          test: /\.(css|scss)$/,
+          use: ["css-loader", "sass-loader"],
         },
         {
           test: /\.(woff|woff2)$/i,
           type: 'asset/resource',
-        },
-        {
-          test: /\.ico$/,
-          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][ext][query]',
+          },
         },
         {
           test: /\.svg$/,
+          exclude: /favicon/, // don't inline favicon.svg
           type: 'asset/inline',
           generator: {
             dataUrl: (content) => {
@@ -56,52 +56,40 @@ module.exports = (env) => {
           },
         },
         {
-          test: /\.webp$/,
+          test: /\.(ico|png|svg|webp|jpg)$/i,
           type: 'asset/resource',
           generator: {
-            filename: 'images/[hash][ext][query]',
-          },
-        },
-        {
-          test: /\.png$/,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[hash][ext][query]',
-          },
-        },
-        {
-          test: /\.jpg$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[hash][ext][query]',
+            filename: 'images/[name].[hash:8][ext][query]',
           },
         },
       ],
     },
     plugins: [
       new Dotenv(),
-      new HtmlWebpackPlugin({
-        title: "output management",
-        template: "./src/index.html",
-        favicon: "./src/favicon.svg",
-        filename: "index.html",
-        inject: "head",
+      new HtmlBundlerPlugin({
+        entry: {
+          // define templates here
+          index: 'src/index.html', // => dist/index.html
+        },
+        js: {
+          // output filename of JS
+          filename: 'js/[name].[contenthash:8].js',
+        },
+        css: {
+          // output filename of CSS
+          filename: 'css/[name].[contenthash:8].css',
+        },
+        preload: [
+          {
+            test: /\.woff2?$/,
+            attributes: { as: 'font', crossorigin: true },
+          },
+        ],
         minify: {
           removeComments: true,
           collapseWhitespace: true,
           removeAttributeQuotes: true,
         },
-      }),
-      new HtmlWebpackInjectPreload({
-        files: [
-          {
-            match: /.*\.woff2$/,
-            attributes: { as: 'font', type: 'font/woff2', crossorigin: true },
-          },
-        ],
-      }),
-      new MiniCssExtractPlugin({
-        filename: "[name].css",
       }),
     ],
     optimization: {
@@ -124,7 +112,6 @@ module.exports = (env) => {
       ],
     },
     output: {
-      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
       clean: true,
     },
